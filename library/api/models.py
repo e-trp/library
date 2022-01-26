e7493e7fdcef3f4aab5a0018a670f76746c7e594
EvgeniyToropchin <etoropchin@gmail.com>
2022-01-26 02:33:29 -0800

    books, author, subs in models

--- library/api/models.py
+++ library/api/models.py
@@ -1,3 +1,23 @@
+from distutils.command.config import LANG_EXT
 from django.db import models
 
 # Create your models here.
+
+
+class Books(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    language = models.CharField(max_length=50, null=False, blank=False)
+    publish_date = models.DateTimeField(null=False, blank=False)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    mid_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+
+
+class Subsciber(models.Model):
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    mid_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+    email = models.EmailField(max_length=255, null=False, blank=False)
