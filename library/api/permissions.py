bb67a35a571a8d5d6c257ffaec5441c6ce02f25f
EvgeniyToropchin <etoropchin@gmail.com>
2022-01-26 23:03:41 -0800

    apply permission in view, update requirements.txt

--- library/api/permissions.py
+++ library/api/permissions.py
@@ -9,7 +9,7 @@ class LibraryPermission(BasePermission):
             curr_year = datetime.date.today().year
             if (curr_year - obj.publish_date.date().year) >= 1:
                 return False
-        return True
+        return request.method in SAFE_METHODS
 
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_authenticated) and request.method in SAFE_METHODS
--- library/api/views.py
+++ library/api/views.py
@@ -1,20 +1,24 @@
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from api.serializers import BookSerializer, AuthorSerializer, SubscriberSerializer
 from api.models import Author, Book, Subscriber
+from rest_framework.permissions import IsAdminUser
+from api.permissions import LibraryPermission
 
 
 
 class AuthorViewSet(ModelViewSet):
+    permission_classes = [LibraryPermission | IsAdminUser]
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
 
 class BookViewSet(ModelViewSet):
+    permission_classes = [LibraryPermission | IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class SubscriberViewSet(ModelViewSet):
+    permission_classes = [LibraryPermission | IsAdminUser]
     queryset = Subscriber.objects.all()
     serializer_class = SubscriberSerializer
